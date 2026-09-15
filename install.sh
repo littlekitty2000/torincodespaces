3155b4dc3260fc5fe1ef0391f766692b79fafe0f
LK2 <littlekitty2000@mailfence.com>
2026-09-15 13:23:44 +1200

    fixed

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "Updating system apt and installing dependencies..."
+echo "Updating system apt and adding official Mozilla repositories..."
+sudo apt update && sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:mozillateam/ppa
+echo -e "Package: *\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 1001" | sudo tee /etc/apt/preferences.d/mozilla-firefox
 sudo apt update && sudo apt install -y tor xvfb xpra firefox-esr
 
 echo "Putting the Tor in the Firefox..."
@@ -12,4 +15,4 @@ user_pref("network.proxy.socks_version", 5);
 user_pref("network.proxy.socks_remote_dns", true);
 EOF
 
-echo "Installed."
+echo "Dependencies installed."
